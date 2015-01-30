7cbdb9b2f9d08e95914c4cbd364dc978229200b2
anurse <andrew@andrewnurse.net>
2015-01-30 13:25:45 -0800

    :white_check_mark: fix some of the path retrival code

--- test/sh/tests/use/Use-ing an alias puts the matching runtime on the path.sh
+++ test/sh/tests/use/Use-ing an alias puts the matching runtime on the path.sh
@@ -10,6 +10,6 @@ $_KVM_COMMAND_NAME use default
 # Check paths
 EXPECTED_ROOT="$KVM_USER_HOME/runtimes/$_KVM_RUNTIME_PACKAGE_NAME-mono.$_TEST_VERSION/bin"
 
-[ $(path_of $_KVM_RUNTIME_EXEC_NAME) == "$EXPECTED_ROOT/$_KVM_RUNTIME_EXEC_NAME" ] || die "'$_KVM_RUNTIME_EXEC_NAME' was not available at the specified path!"
-[ $(path_of $_KVM_RUNTIME_HOST_NAME) == "$EXPECTED_ROOT/$_KVM_RUNTIME_HOST_NAME" ] || die "'$_KVM_RUNTIME_HOST_NAME' was not available at the specified path!"
-[ $(path_of $_KVM_PACKAGE_MANAGER_NAME) == "$EXPECTED_ROOT/$_KVM_PACKAGE_MANAGER_NAME" ] || die "'$_KVM_PACKAGE_MANAGER_NAME' was not available at the specified path!"
+[ "$(path_of $_KVM_RUNTIME_EXEC_NAME)" == "$EXPECTED_ROOT/$_KVM_RUNTIME_EXEC_NAME" ] || die "'$_KVM_RUNTIME_EXEC_NAME' was not available at the specified path!"
+[ "$(path_of $_KVM_RUNTIME_HOST_NAME)" == "$EXPECTED_ROOT/$_KVM_RUNTIME_HOST_NAME" ] || die "'$_KVM_RUNTIME_HOST_NAME' was not available at the specified path!"
+[ "$(path_of $_KVM_PACKAGE_MANAGER_NAME)" == "$EXPECTED_ROOT/$_KVM_PACKAGE_MANAGER_NAME" ] || die "'$_KVM_PACKAGE_MANAGER_NAME' was not available at the specified path!"
--- test/sh/tests/use/Use-ing none clears runtime from the PATH.sh
+++ test/sh/tests/use/Use-ing none clears runtime from the PATH.sh
@@ -10,6 +10,6 @@ $_KVM_COMMAND_NAME use none
 # Check paths
 EXPECTED_ROOT="$KVM_USER_HOME/runtimes/$_KVM_RUNTIME_PACKAGE_NAME-mono.$_TEST_VERSION/bin"
 
-[ $(path_of $_KVM_RUNTIME_EXEC_NAME) != "$EXPECTED_ROOT/$_KVM_RUNTIME_EXEC_NAME" ] || die "'$_KVM_RUNTIME_EXEC_NAME' was still available at the expected path!"
-[ $(path_of $_KVM_RUNTIME_HOST_NAME) != "$EXPECTED_ROOT/$_KVM_RUNTIME_HOST_NAME" ] || die "'$_KVM_RUNTIME_HOST_NAME' was still available at the expected path!"
-[ $(path_of $_KVM_PACKAGE_MANAGER_NAME) != "$EXPECTED_ROOT/$_KVM_PACKAGE_MANAGER_NAME" ] || die "'$_KVM_PACKAGE_MANAGER_NAME' was still available at the expected path!"
+[ "$(path_of $_KVM_RUNTIME_EXEC_NAME)" != "$EXPECTED_ROOT/$_KVM_RUNTIME_EXEC_NAME" ] || die "'$_KVM_RUNTIME_EXEC_NAME' was still available at the expected path!"
+[ "$(path_of $_KVM_RUNTIME_HOST_NAME)" != "$EXPECTED_ROOT/$_KVM_RUNTIME_HOST_NAME" ] || die "'$_KVM_RUNTIME_HOST_NAME' was still available at the expected path!"
+[ "$(path_of $_KVM_PACKAGE_MANAGER_NAME)" != "$EXPECTED_ROOT/$_KVM_PACKAGE_MANAGER_NAME" ] || die "'$_KVM_PACKAGE_MANAGER_NAME' was still available at the expected path!"
