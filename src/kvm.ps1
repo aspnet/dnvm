1f46c4576ea6e98b2e7a532f43ea153dd7735e0b
anurse <andrew@andrewnurse.net>
2015-02-04 15:18:56 -0800

    port install command

--- src/kvm.ps1
+++ src/kvm.ps1
@@ -11,6 +11,10 @@ Set-Variable -Option Constant "CommandName" ([IO.Path]::GetFileNameWithoutExtens
 Set-Variable -Option Constant "CommandFriendlyName" "K Runtime Version Manager"
 Set-Variable -Option Constant "DefaultUserDirectoryName" ".k"
 Set-Variable -Option Constant "RuntimePackageName" "kre"
+Set-Variable -Option Constant "DefaultFeed" "https://www.myget.org/F/aspnetvnext/api/v2"
+Set-Variable -Option Constant "CrossGenCommand" "k-crossgen"
+
+Set-Variable -Option Constant "OptionPadding" 20
 
 # Commands that have been deprecated but do still work.
 $DeprecatedCommands = @("unalias")
@@ -18,16 +22,20 @@ $DeprecatedCommands = @("unalias")
 # Load Environment variables
 $RuntimeHomes = $env:KRE_HOME
 $UserHome = $env:KRE_USER_HOME
+$ActiveFeed = $env:KRE_FEED
 
 # Default Exit Code
 $Script:ExitCode = 0
-
 
 ############################################################
 ### Below this point, the terms "KVM", "KRE", "K", etc.  ###
 ### should never be used. Instead, use the Constants     ###
 ### defined above                                        ###
 ############################################################
+
+if(!$ActiveFeed) {
+    $ActiveFeed = $DefaultFeed
+}
 
 # Determine where runtimes can exist (RuntimeHomes)
 if(!$RuntimeHomes) {
@@ -64,7 +72,8 @@ Write-Debug "Running $CommandName"
 Write-Debug "Runtime Homes: $RuntimeHomes"
 Write-Debug "User Home: $UserHome"
 
-$AliasesDir = "$UserHome\alias"
+$AliasesDir = Join-Path $UserHome "alias"
+$RuntimesDir = Join-Path $UserHome "runtimes"
 $Aliases = $null
 
 ### Helper Functions
@@ -96,11 +105,26 @@ function IsOnPath {
     $env:Path.Split(';') -icontains $dir
 }
 
+function Get-RuntimeId(
+    [Parameter(Mandatory=$true)][string]$Architecture,
+    [Parameter(Mandatory=$true)][string]$Runtime) {
+
+    "$RuntimePackageName-$Runtime-win-$Architecture"
+}
+
 function Get-RuntimeName(
     [Parameter(Mandatory=$true)][string]$Version,
     [Parameter(Mandatory=$true)][string]$Architecture,
     [Parameter(Mandatory=$true)][string]$Runtime) {
-    "$RuntimePackageName-$Runtime-win-$Architecture.$Version"
+
+    $aliasPath = Join-Path $AliasesDir "$Version.txt"
+
+    if(Test-Path $aliasPath) {
+        Get-Content $aliasPath
+    }
+    else {
+        "$(Get-RuntimeId $Architecture $Runtime).$Version"
+    }
 }
 
 filter List-Parts {
@@ -140,7 +164,7 @@ function Read-Alias($Name) {
 
     $aliases = Get-RuntimeAlias
 
-    $result = @($aliases | Where { !$Name -or ($_.Alias.Contains($Name)) })
+    $result = @($aliases | Where-Object { !$Name -or ($_.Alias.Contains($Name)) })
     if($Name -and ($result.Length -eq 1)) {
         Write-Console "Alias '$Name' is set to '$($result[0].Name)'"
     } elseif($Name -and ($result.Length -eq 0)) {
@@ -186,6 +210,132 @@ function Delete-Alias {
     }
 }
 
+function Apply-Proxy {
+param(
+  [System.Net.WebClient] $wc,
+  [string]$Proxy
+)
+  if (!$Proxy) {
+    $Proxy = $env:http_proxy
+  }
+  if ($Proxy) {
+    $wp = New-Object System.Net.WebProxy($Proxy)
+    $pb = New-Object UriBuilder($Proxy)
+    if (!$pb.UserName) {
+        $wp.Credentials = [System.Net.CredentialCache]::DefaultCredentials
+    } else {
+        $wp.Credentials = New-Object System.Net.NetworkCredential($pb.UserName, $pb.Password)
+    }
+    $wc.Proxy = $wp
+  }
+}
+
+function Find-Latest {
+    param(
+        [string]$platform,
+        [string]$architecture,
+        [string]$Feed,
+        [string]$Proxy
+    )
+    if(!$Feed) { $Feed = $ActiveFeed }
+
+    Write-Console "Determining latest version"
+
+    $url = "$Feed/GetUpdates()?packageIds=%27$RuntimePackageName-$platform-win-$architecture%27&versions=%270.0%27&includePrerelease=true&includeAllVersions=false"
+
+    # NOTE: DO NOT use Invoke-WebRequest. It requires PowerShell 4.0!
+
+    $wc = New-Object System.Net.WebClient
+    Apply-Proxy $wc -Proxy:$Proxy
+    Write-Debug "Downloading $url ..."
+    [xml]$xml = $wc.DownloadString($url)
+
+    $version = Select-Xml "//d:Version" -Namespace @{d='http://schemas.microsoft.com/ado/2007/08/dataservices'} $xml
+
+    if (![String]::IsNullOrWhiteSpace($version)) {
+        $version
+    }
+}
+
+function Get-PackageVersion() {
+    param(
+        [string] $runtimeFullName
+    )
+    return $runtimeFullName -replace '[^.]*.(.*)', '$1'
+}
+
+function Get-PackageRuntime() {
+    param(
+        [string] $runtimeFullName
+    )
+    return $runtimeFullName -replace "$RuntimePackageName-([^-]*).*", '$1'
+}
+
+function Get-PackageArch() {
+    param(
+        [string] $runtimeFullName
+    )
+    return $runtimeFullName -replace "$RuntimePackageName-[^-]*-[^-]*-([^.]*).*", '$1'
+}
+
+function Download-Package(
+    [string]$Version,
+    [string]$Architecture,
+    [string]$Runtime,
+    [string]$DestinationFile,
+    [string]$Feed,
+    [string]$Proxy) {
+
+    if(!$Feed) { $Feed = $ActiveFeed }
+    
+    $url = "$Feed/package/" + (Get-RuntimeId $Architecture $Runtime) + "/" + $Version
+    
+    Write-Console "Downloading $runtimeFullName from $feed"
+
+    $wc = New-Object System.Net.WebClient
+    Apply-Proxy $wc -Proxy:$Proxy
+    Write-Debug "Downloading $url ..."
+    $wc.DownloadFile($url, $DestinationFile)
+}
+
+function Unpack-Package([string]$DownloadFile, [string]$UnpackFolder) {
+    Write-Debug "Unpacking $DownloadFile to $UnpackFolder"
+
+    $compressionLib = [System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem')
+
+    if($compressionLib -eq $null) {
+      try {
+          # Shell will not recognize nupkg as a zip and throw, so rename it to zip
+          $runtimeZip = [System.IO.Path]::ChangeExtension($DownloadFile, "zip")
+          Rename-Item $runtimeFile $runtimeZip
+          # Use the shell to uncompress the nupkg
+          $shell_app=new-object -com shell.application
+          $zip_file = $shell_app.namespace($runtimeZip)
+          $destination = $shell_app.namespace($UnpackFolder)
+          $destination.Copyhere($zip_file.items(), 0x14) #0x4 = don't show UI, 0x10 = overwrite files
+      }
+      finally {
+        # Clean up the package file itself.
+        Remove-Item $runtimeZip -Force
+      }
+    } else {
+        [System.IO.Compression.ZipFile]::ExtractToDirectory($DownloadFile, $UnpackFolder)
+        
+        # Clean up the package file itself.
+        Remove-Item $DownloadFile -Force
+    }
+
+    If (Test-Path -LiteralPath ($UnpackFolder + "\[Content_Types].xml")) {
+        Remove-Item -LiteralPath ($UnpackFolder + "\[Content_Types].xml")
+    }
+    If (Test-Path ($UnpackFolder + "\_rels\")) {
+        Remove-Item -LiteralPath ($UnpackFolder + "\_rels\") -Force -Recurse
+    }
+    If (Test-Path ($UnpackFolder + "\package\")) {
+        Remove-Item -LiteralPath ($UnpackFolder + "\package\") -Force -Recurse
+    }
+}
+
 ### Commands
 
 <#
@@ -222,9 +372,12 @@ function command-help {
 
                         $paramStr = "";
                         if($_.position -eq "Named") {
-                            $paramStr += "-$name "
+                            $paramStr += "-$name"
                         }
                         if($_.parameterValue) {
+                            if($_.position -eq "Named") {
+                                $paramStr += " "
+                            }
                             $paramStr += "<$($_.name)>"
                         }
 
@@ -251,13 +404,16 @@ function command-help {
                     } else {
                         $name="<$name>"
                     }
-                    Write-Console "  $($name.PadRight(10)) $($_.description.Text)"
+                    Write-Console "  $($name.PadRight($OptionPadding)) $($_.description.Text)"
                 }
             }
 
             if($help.description) {
                 Write-Console
-                Write-Console $help.description.Text
+                Write-Console "remarks:"
+                Write-Console (
+                    $help.description.Text.Split(@("`r", "`n"), "RemoveEmptyEntries") | 
+                        ForEach-Object { "  $_" })
             }
 
             if($DeprecatedCommands -contains $Command) {
@@ -335,7 +491,7 @@ function command-alias {
         [Parameter(ParameterSetName="Write",Mandatory=$true,Position=1)]
         [string]$Version,
 
-        [Alias("a")]
+        [Alias("arch")]
         [ValidateSet("x86","amd64")]
         [Parameter(ParameterSetName="Write", Mandatory=$false)]
         [string]$Architecture = "x86",
@@ -363,6 +519,144 @@ function command-unalias {
         [Parameter(Mandatory=$true,Position=0)][string]$Name)
     Write-Warning "This command is obsolete. Use '$CommandName alias -d' instead"
     command-alias -Delete -Name $Name
+}
+
+<#
+.SYNOPSIS
+    Installs a version of the runtime
+.PARAMETER VersionOrNuPkg
+    The version to install from the current channel, the path to a '.nupkg' file to install, or 'latest' to
+    install the latest available version from the current channel.
+.PARAMETER Architecture
+    The processor architecture of the runtime to install (default: x86)
+.PARAMETER Runtime
+    The runtime flavor to install (default: clr)
+.PARAMETER Alias
+    Set alias <Alias> to the installed runtime
+.PARAMETER Force
+    Overwrite an existing runtime if it already exists
+.PARAMETER Proxy
+    Use the given address as a proxy when accessing remote server
+.Parameter NoNative
+    Skip generation of native images when installing coreclr runtime flavors
+
+.DESCRIPTION
+    A proxy can also be specified by using the 'http_proxy' environment variable
+
+#>
+function command-install {
+    param(
+        [Parameter(Mandatory=$false, Position=0)]
+        [string]$VersionOrNuPkg,
+
+        [Alias("arch")]
+        [ValidateSet("x86","x64")]
+        [Parameter(Mandatory=$false)]
+        [string]$Architecture = "x86",
+
+        [Alias("r")]
+        [ValidateSet("clr","coreclr")]
+        [Parameter(Mandatory=$false)]
+        [string]$Runtime = "clr",
+
+        [Alias("a")]
+        [Parameter(Mandatory=$false)]
+        [string]$Alias,
+
+        [Alias("f")]
+        [Parameter(Mandatory=$false)]
+        [switch]$Force,
+
+        [Parameter(Mandatory=$false)]
+        [string]$Proxy,
+
+        [Parameter(Mandatory=$false)]
+        [switch]$NoNative)
+
+    if(!$VersionOrNuPkg) {
+        Write-Warning "A version, nupkg path, or the string 'latest' must be provided."
+        command-help install
+        return
+    }
+
+    if ($VersionOrNuPkg -eq "latest") {
+        $VersionOrNuPkg = Find-Latest $Runtime $Architecture
+    }
+
+    $IsNuPkg = $VersionOrNuPkg.EndsWith(".nupkg")
+
+    if ($IsNuPkg) {
+        if(!(Test-Path $VersionOrNuPkg)) {
+            throw "Unable to locate package file: '$VersionOrNuPkg'"
+        }
+        $runtimeFullName = [System.IO.Path]::GetFileNameWithoutExtension($VersionOrNuPkg)
+        $Architecture = Get-PackageArch $runtimeFullName
+        $Runtime = Get-PackageRuntime $runtimeFullName
+    } else {
+        $runtimeFullName = Get-RuntimeName $VersionOrNuPkg $Architecture $Runtime
+    }
+
+    Write-Debug "Preparing to install runtime '$runtimeFullName'"
+    Write-Debug "Architecture: $Architecture"
+    Write-Debug "Runtime: $Runtime"
+
+    $RuntimeFolder = Join-Path $RuntimesDir $runtimeFullName
+    Write-Debug "Destination: $RuntimeFolder"
+
+    if((Test-Path $RuntimeFolder) -and $Force) {
+        Write-Console "Cleaning existing installation..."
+        Remove-Item $RuntimeFolder -Recurse -Force
+    }
+
+    if(Test-Path $RuntimeFolder) {
+        Write-Warning "Target folder '$RuntimeFolder' already exists"
+        $Script:ExitCode = 1
+        return
+    }
+
+    $UnpackFolder = Join-Path $RuntimesDir "temp"
+    $DownloadFile = Join-Path $UnpackFolder "$runtimeFullName.nupkg"
+
+    if(Test-Path $UnpackFolder) {
+        Write-Debug "Cleaning temporary directory $UnpackFolder"
+        Remove-Item $UnpackFolder -Recurse -Force
+    }
+    New-Item -Type Directory $UnpackFolder | Out-Null
+
+    if($IsNuPkg) {
+        Copy-Item $VersionOrNuPkg $DownloadFile
+    } else {
+        # Download the package
+        Download-Package $VersionOrNuPkg $Architecture $Runtime $DownloadFile -Proxy:$Proxy
+    }
+
+    Unpack-Package $DownloadFile $UnpackFolder
+
+    New-Item -Type Directory $RuntimeFolder -Force | Out-Null
+    Write-Console "Installing to $RuntimeFolder"
+    Write-Debug "Moving package contents to $RuntimeFolder"
+    Move-Item "$UnpackFolder\*" $RuntimeFolder
+    Write-Debug "Cleaning temporary directory $UnpackFolder"
+    Remove-Item $UnpackFolder -Force | Out-Null
+
+    $PackageVersion = Get-PackageVersion $runtimeFullName
+
+    command-use $PackageVersion
+
+    if($Alias) {
+        command-alias $Alias $PackageVersion
+    }
+
+    if ($runtimeFullName.Contains("CoreCLR")) {
+        if ($NoNative) {
+          Write-Console "Skipping native image compilation."
+        }
+        else {
+          Write-Console "Compiling native images for $runtimeFullName to improve startup performance..."
+          Start-Process $CrossGenCommand -Wait
+          Write-Console "Finished native image compilation."
+        }
+    }
 }
 
 ### The main "entry point"
