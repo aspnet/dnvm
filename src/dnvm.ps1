aspnet/dnvm

commit 828b00804650a8b77bf324c2f24ca4270b09ef7f

dnvm install / upgrade should put the installed runtime on path even if the runtime is already installed
Fixes: https://github.com/aspnet/dnvm/issues/199

src/dnvm.ps1
@@ -1038,8 +1038,6 @@ function dnvm-install {
         _WriteDebug "Cleaning temporary directory $UnpackFolder"
         Remove-Item $UnpackFolder -Force | Out-Null
 
-        dnvm-use $PackageVersion -Architecture:$Architecture -Runtime:$Runtime -Persistent:$Persistent
-
         if ($Runtime -eq "clr") {
             if (-not $NoNative) {
                 if ((Is-Elevated) -or $Ngen) {
@@ -1072,6 +1070,8 @@ function dnvm-install {
             _WriteOut "Unexpected platform: $Runtime. No optimization would be performed on the package installed."
         }
     }
+
+    dnvm-use $PackageVersion -Architecture:$Architecture -Runtime:$Runtime -Persistent:$Persistent
 
     if($Alias) {
         _WriteDebug "Aliasing installed runtime to '$Alias'"

test/ps1/tests/Install.Tests.ps1
@@ -77,7 +77,7 @@ Describe "install" -Tag "install" {
         $runtimeName = GetRuntimeName -clr CLR -arch x86
 
         It "uses x86" {
-            $__dnvmtest_out.Trim() | Should Be "'$runtimeName' is already installed."
+            $__dnvmtest_out.Trim() | Should Be "'$runtimeName' is already installed.`r`nAdding $UserPath\runtimes\$runtimeName\bin to process PATH"
         }
     }
 
@@ -86,7 +86,7 @@ Describe "install" -Tag "install" {
         $runtimeName = GetRuntimeName -clr CLR -arch x86
 
         It "uses Desktop CLR" {
-            $__dnvmtest_out.Trim() | Should Be "'$runtimeName' is already installed."
+            $__dnvmtest_out.Trim() | Should Be "'$runtimeName' is already installed.`r`nAdding $UserPath\runtimes\$runtimeName\bin to process PATH"
         }
     }
 
@@ -95,7 +95,7 @@ Describe "install" -Tag "install" {
         $runtimeName = GetRuntimeName -clr CLR -arch x86
 
         It "uses x86/Desktop" {
-            $__dnvmtest_out.Trim() | Should Be "'$runtimeName' is already installed."
+            $__dnvmtest_out.Trim() | Should Be "'$runtimeName' is already installed.`r`nAdding $UserPath\runtimes\$runtimeName\bin to process PATH"
         }
     }
 
@@ -128,7 +128,7 @@ Describe "install" -Tag "install" {
         $runtimePath = "$UserPath\runtimes\$runtimeName"
         It "ensures the runtime is installed" {
             __dnvmtest_run install $TestRuntimeVersion -arch x86 -r "CLR" | Out-Null
-            $__dnvmtest_out.Trim() | Should Be "'$runtimeName' is already installed."
+            $__dnvmtest_out.Trim() | Should Be "'$runtimeName' is already installed.`r`nAdding $UserPath\runtimes\$runtimeName\bin to process PATH"
             $runtimePath | Should Exist
         }
     }

commit 703068b211e978212c4891015b484055f4e2d9c1

Regression causing file not found error when installing coreclr dnx
This is a regression introduced by https://github.com/aspnet/dnvm/commit/828b00804650a8b77bf324c2f24ca4270b09ef7f.

On install of coreclr xre previously the dnvm use happened before invoking cross gen. With this change I moved this to a common place so that dnvm-use is called irrespective if the dnx already exists or not.

src/dnvm.ps1
@@ -1004,6 +1004,7 @@ function dnvm-install {
 
     if(Test-Path $RuntimeFolder) {
         _WriteOut "'$runtimeFullName' is already installed."
+        dnvm-use $PackageVersion -Architecture:$Architecture -Runtime:$Runtime -Persistent:$Persistent
     }
     else {
         $Architecture = GetArch $Architecture
@@ -1038,6 +1039,8 @@ function dnvm-install {
         _WriteDebug "Cleaning temporary directory $UnpackFolder"
         Remove-Item $UnpackFolder -Force | Out-Null
 
+        dnvm-use $PackageVersion -Architecture:$Architecture -Runtime:$Runtime -Persistent:$Persistent
+
         if ($Runtime -eq "clr") {
             if (-not $NoNative) {
                 if ((Is-Elevated) -or $Ngen) {
@@ -1070,8 +1073,6 @@ function dnvm-install {
             _WriteOut "Unexpected platform: $Runtime. No optimization would be performed on the package installed."
         }
     }
-
-    dnvm-use $PackageVersion -Architecture:$Architecture -Runtime:$Runtime -Persistent:$Persistent
 
     if($Alias) {
         _WriteDebug "Aliasing installed runtime to '$Alias'"