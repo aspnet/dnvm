7aa35c4719c409d4d075f91f8ab9cab51657c94c
Brennan <brecon@microsoft.com>
2015-07-27 16:45:51 -0700

    Fix dnvm install using wrong value for version

--- src/dnvm.ps1
+++ src/dnvm.ps1
@@ -1234,7 +1234,7 @@ function dnvm-install {
         $Architecture = Get-PackageArch $runtimeFullName
         $Runtime = Get-PackageRuntime $runtimeFullName
         $OS = Get-PackageOS $runtimeFullName
-        $Version = Get-PackageVersion $VersionNuPkgOrAlias
+        $Version = Get-PackageVersion $runtimeFullName
     } else {
         $aliasPath = Join-Path $AliasesDir "$VersionNuPkgOrAlias$AliasExtension"
         if(Test-Path $aliasPath) {
@@ -1258,7 +1258,7 @@ function dnvm-install {
         }
     }
 
-    $runtimeInfo = GetRuntimeInfo $Architecture $Runtime $OS $VersionNupkgOrAlias
+    $runtimeInfo = GetRuntimeInfo $Architecture $Runtime $OS $Version
 
     if ($VersionNuPkgOrAlias -eq "latest") {
         Write-Progress -Activity "Installing runtime" -Status "Determining latest runtime" -Id 1
