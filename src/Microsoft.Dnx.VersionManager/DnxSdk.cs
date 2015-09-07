21aff4edaec3a1c3c1c52fa5bb951338c791cb28
David Fowler <davidfowl@gmail.com>
2015-09-06 17:29:31 -0700

    Added parsing for dnx names

---
+++ src/Microsoft.Dnx.VersionManager/DnxSdk.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Dnx.VersionManager
+{
+    public class DnxSdk
+    {
+        public string Version { get; set; }
+
+        public string Flavor { get; set; }
+
+        public string Architecture { get; set; }
+
+        public string OperationSystem { get; set; }
+
+        public string Location { get; set; }
+
+        public string FullName { get; set; }
+
+        public static string GetRuntimeHome()
+        {
+            var homePath = Environment.ExpandEnvironmentVariables(Environment.GetEnvironmentVariable("DNX_HOME"));
+
+            if (string.IsNullOrEmpty(homePath))
+            {
+                var basePath = Environment.GetEnvironmentVariable("HOME");
+                if (string.IsNullOrEmpty(basePath))
+                {
+                    basePath = Environment.GetEnvironmentVariable("USERPROFILE");
+                }
+
+                homePath = Path.Combine(basePath, ".dnx");
+            }
+
+            return homePath;
+        }
+
+        public static DnxSdk GetRuntime(string fullName)
+        {
+            string flavor;
+            string os;
+            string arch;
+            string version;
+            if (!TryParseFullName(fullName, out flavor, out os, out arch, out version))
+            {
+                return null;
+            }
+
+            return GetRuntime(version, flavor, os, arch);
+        }
+
+        public static DnxSdk GetRuntime(string version, string flavor, string os, string arch)
+        {
+            return GetRuntime(GetRuntimeHome(), version, flavor, os, arch);
+        }
+
+        public static DnxSdk GetRuntime(string basePath, string version, string flavor, string os, string arch)
+        {
+            var fullName = GetRuntimeName(flavor, os, arch) + $".{version}";
+            return new DnxSdk
+            {
+                FullName = fullName,
+                Location = Path.Combine(basePath, "runtimes", fullName),
+                Architecture = arch,
+                Flavor = flavor,
+                OperationSystem = os,
+                Version = version
+            };
+        }
+
+        private static bool TryParseFullName(string fullName, out string flavor, out string os, out string arch, out string version)
+        {
+            flavor = null;
+            os = null;
+            arch = null;
+            version = null;
+
+            var tokenBuilder = new StringBuilder();
+
+            // e.g. dnx-clr-win-x64.1.0.0-dev
+            // e.g dnx-mono.1.0.0-dev
+            var state = DnxNameState.Dnx;
+            foreach (var ch in fullName)
+            {
+                if (ch == '-')
+                {
+                    if (state == DnxNameState.Dnx)
+                    {
+                        var runtime = tokenBuilder.ToString();
+
+                        if (!string.Equals(runtime, "dnx", StringComparison.OrdinalIgnoreCase))
+                        {
+                            return false;
+                        }
+
+                        state = DnxNameState.Flavor;
+                        tokenBuilder.Clear();
+                    }
+                    else if (state == DnxNameState.Flavor)
+                    {
+                        flavor = tokenBuilder.ToString();
+
+                        if (!IsValidFlavor(flavor))
+                        {
+                            return false;
+                        }
+
+                        state = DnxNameState.Os;
+                        tokenBuilder.Clear();
+                    }
+                    else if (state == DnxNameState.Os)
+                    {
+                        os = tokenBuilder.ToString();
+
+                        if (!IsValidOS(os))
+                        {
+                            return false;
+                        }
+
+                        state = DnxNameState.Arch;
+                        tokenBuilder.Clear();
+                    }
+                    else
+                    {
+                        tokenBuilder.Append(ch);
+                    }
+                }
+                else if (ch == '.')
+                {
+                    if (state == DnxNameState.Arch)
+                    {
+                        arch = tokenBuilder.ToString();
+
+                        if (!IsValidArch(arch))
+                        {
+                            return false;
+                        }
+
+                        state = DnxNameState.Version;
+                        tokenBuilder.Clear();
+
+                        continue;
+                    }
+                    else if (state == DnxNameState.Flavor)
+                    {
+                        flavor = tokenBuilder.ToString();
+
+                        if (string.Equals(flavor, "mono", StringComparison.OrdinalIgnoreCase))
+                        {
+                            state = DnxNameState.Version;
+                            tokenBuilder.Clear();
+
+                            continue;
+                        }
+                    }
+
+                    tokenBuilder.Append(ch);
+                }
+                else
+                {
+                    tokenBuilder.Append(ch);
+                }
+            }
+
+            if (state != DnxNameState.Version)
+            {
+                return false;
+            }
+
+            version = tokenBuilder.ToString();
+
+            return true;
+        }
+
+        private static bool IsValidFlavor(string flavor)
+        {
+            switch (flavor.ToLowerInvariant())
+            {
+                case "mono":
+                case "clr":
+                case "coreclr":
+                    return true;
+            }
+            return false;
+        }
+
+        private static bool IsValidArch(string arch)
+        {
+            switch (arch.ToLowerInvariant())
+            {
+                case "arm":
+                case "x86":
+                case "x64":
+                    return true;
+            }
+            return false;
+        }
+
+        private static bool IsValidOS(string os)
+        {
+            switch (os.ToLowerInvariant())
+            {
+                case "linux":
+                case "darwin":
+                case "win":
+                    return true;
+            }
+            return false;
+        }
+
+        private static string GetRuntimeName(string flavor, string os, string architecture)
+        {
+            // Mono ignores os and architecture
+            if (string.Equals(flavor, "mono", StringComparison.OrdinalIgnoreCase))
+            {
+                return "dnx-mono";
+            }
+
+            return $"dnx-{flavor}-{os}-{architecture}";
+        }
+
+        private enum DnxNameState
+        {
+            Dnx,
+            Flavor,
+            Os,
+            Arch,
+            Version
+        }
+    }
+}
---
+++ test/Microsoft.Dnx.VersionManager.Tests/DnxSdkFacts.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.Dnx.VersionManager.Tests
+{
+    public class DnxSdkFacts
+    {
+        [Theory]
+        [InlineData("dnx-clr-win-x86.1.0.0-dev")]
+        [InlineData("dnx-clr-win-x64.1.0.0-dev")]
+        [InlineData("dnx-coreclr-win-arm.1.0.0-dev")]
+        [InlineData("dnx-coreclr-win-x86.1.0.0-dev")]
+        [InlineData("dnx-coreclr-win-x64.1.0.0-dev")]
+        [InlineData("dnx-coreclr-linux-x64.1.0.0-dev")]
+        [InlineData("dnx-coreclr-darwin-x64.1.0.0-dev")]
+        [InlineData("dnx-coreclr-darwin-x64.1.0.0-dev")]
+        [InlineData("dnx-mono.1.0.0-dev")]
+        public void GetRuntimeFromFullNameValidName(string fullName)
+        {
+            var sdk = DnxSdk.GetRuntime(fullName);
+
+            Assert.NotNull(sdk);
+            Assert.Equal("1.0.0-dev", sdk.Version);
+            Assert.Equal(fullName, sdk.FullName);
+        }
+
+        [Fact]
+        public void GetRuntimeFromFullNameValidateProperties()
+        {
+            var sdk = DnxSdk.GetRuntime("dnx-clr-win-x86.1.0.0-dev");
+
+            Assert.NotNull(sdk);
+            Assert.Equal("clr", sdk.Flavor);
+            Assert.Equal("win", sdk.OperationSystem);
+            Assert.Equal("x86", sdk.Architecture);
+            Assert.Equal("1.0.0-dev", sdk.Version);
+        }
+
+        [Theory]
+        [InlineData("foo")]
+        [InlineData("dnx----")]
+        [InlineData("dnx-dev-ol-8")]
+        [InlineData("dnx-x86-win-.os")]
+        [InlineData("---.")]
+        [InlineData("dnx-1-2-3-4-5-5")]
+        [InlineData("dnx-.3.4.-.4.4.")]
+        [InlineData("something-clr-win-x86.1.0.0-dev")]
+        public void GetRuntimeFromFullNameBadFormat(string fullname)
+        {
+            var sdk = DnxSdk.GetRuntime(fullname);
+
+            Assert.Null(sdk);
+        }
+    }
+}
