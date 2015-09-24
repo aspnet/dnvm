0a3b6e86a53106dd988586b0dc1feb2bacc24850
Martin Björkström <martin.bjorkstrom@gmail.com>
2015-09-24 22:21:51 +0300

    Added 'dnvm alias -d' and deprecated 'dnvm unalias'

--- test/sh/tests/alias/Aliases can be removed.sh
+++ test/sh/tests/alias/Aliases can be removed.sh
@@ -5,7 +5,7 @@ source $_DNVM_PATH
 $_DNVM_COMMAND_NAME alias test_alias_unalias "$_TEST_VERSION"
 
 # Unalias it
-$_DNVM_COMMAND_NAME unalias test_alias_unalias
+$_DNVM_COMMAND_NAME alias -d test_alias_unalias
 
 # Check the alias file
 [ ! -e "$DNX_USER_HOME/alias/test_alias_unalias.alias" ] || die "test alias was not removed"
